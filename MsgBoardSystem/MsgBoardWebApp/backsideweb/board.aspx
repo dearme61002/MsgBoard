e37faa75abf64850b26a4d1382e46f046898b2e1
昶志 林 <dearme61002@gmail.com>
2021-08-28 16:05:20 +0800

    布告欄增加標題按鈕

--- MsgBoardSystem/MsgBoardWebApp/backsideweb/board.aspx
+++ MsgBoardSystem/MsgBoardWebApp/backsideweb/board.aspx
@@ -2,4 +2,23 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <%--開始製作增加標題按鈕--%>
+    <div class="container">
+  <div class="row">
+    <div class="col-2">
+        
+      <div type="text" class="btn alert-dark" style="width:100%">標題</div>
+    </div>
+    <div class="col-8">
+      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+    </div>
+    <div class="col-2">
+     <button type="button" class="btn btn-primary">增加</button>
+    </div>
+  </div>
+</div>
+
+
+
+
 </asp:Content>
--- MsgBoardSystem/MsgBoardWebApp/backsideweb/board.aspx.cs
+++ MsgBoardSystem/MsgBoardWebApp/backsideweb/board.aspx.cs
@@ -9,9 +9,25 @@ namespace MsgBoardWebApp.backsideweb
 {
     public partial class board : System.Web.UI.Page
     {
+        public string dataID;
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                if (Session["UID"] != null)
+                {
+                    dataID = Session["UID"].ToString();
+                }
+                else
+                {
+                    Response.Cookies[".ASPXAUTH"].Expires = DateTime.Now.AddDays(-1);
+                    Response.Redirect(@"~/Page02Login.aspx");
+                }
+            }
+            catch (Exception)
+            {
 
+            }
         }
     }
 }
