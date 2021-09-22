d07fc012c1662041037e1c968b77181021f4d6c3
RickLiu <mushiking02134tc@gmail.com>
2021-09-22 23:35:27 +0800

    # 隱藏登入相關功能列表

--- MsgBoardSystem/MsgBoardWebApp/Main.Master.cs
+++ MsgBoardSystem/MsgBoardWebApp/Main.Master.cs
@@ -13,14 +13,21 @@ namespace MsgBoardWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.guestFunc.Visible = true;
             bool isAuth = HttpContext.Current.Request.IsAuthenticated;//要否讀取COOKie 有給true
-            var user = HttpContext.Current.User;//判讀User
+            var user = HttpContext.Current.User;//判讀User            
 
             if (isAuth && user != null)
             {
                 var identity = HttpContext.Current.User.Identity as FormsIdentity; //判讀Identity
                 if (identity != null)
                 {
+                    // show login functions list 
+                    this.LoginList.Visible = true;
+                    this.loginFunc.Visible = true;
+                    this.guestFunc.Visible = false;
+
+                    // check is admin and show back side website
                     if(AuthManager.UserLevelAuthentication(identity.Ticket.UserData))
                         this.backSide.Visible = true;
                 }
